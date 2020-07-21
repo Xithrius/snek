599692f0fcd7d46f45e7a3396d8f8bfda8d21cf5
Dennis Pham <dennis@dennispham.me>
2020-07-21 21:00:59 +0000

    Create the base syncer ABC class

---
+++ snek/cogs/syncer/syncers/base.py
@@ -0,0 +1,23 @@
+from abc import ABC
+from collections import namedtuple
+
+import discord
+
+Diff = namedtuple('Diff', ('created', 'updated', 'deleted'))
+
+
+class ObjectSyncerABC(ABC):
+    """Base class for synchronising the database with Discord objects in the cache."""
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """The name of the syncer."""
+
+    @abstractmethod
+    async def get_diff(self, guild: discord.Guild) -> None:
+        """Return the difference between the cache of `guild` and the database."""
+
+    @abstractmethod
+    async def sync(self, diff: Diff) -> None:
+        """Perform the API calls for synchronisation."""
