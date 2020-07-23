75a138c0f52b604d009aae5173ee5430e3c80754
Dennis Pham <dennis@dennispham.me>
2020-07-23 19:55:43 +0000

    Extension Manager: Create the Extension converter

--- snek/exts/management/extension_manager.py
+++ snek/exts/management/extension_manager.py
@@ -2,6 +2,7 @@ import logging
 from pkgutil import iter_modules
 
 from discord.ext import commands
+from discord.ext.commands import Context
 
 log = logging.getLogger(__name__)
 
@@ -9,6 +10,29 @@ UNLOAD_BLACKLIST = {'snek.exts.management'}
 EXTENSIONS = frozenset(
     ext.name for ext in iter_modules(('snek/exts',), 'snek.exts.')
 )
+
+
+class Extension(commands.Converter):
+    """
+    Ensure the extension exists and return the full extension path.
+
+    The `*` symbol represents all extensions.
+    """
+
+    async def convert(self, ctx: Context, argument: str) -> str:
+        """Ensure the extension exists and return the full extension path."""
+        if argument == '*':
+            return argument
+
+        argument = argument.lower()
+
+        if '.' not in argument:
+            argument = f'snek.exts.{argument}'
+
+        if argument in EXTENSIONS:
+            return argument
+
+        raise commands.BadArgument(f'❌ Could not find the extension `{argument}`.')
 
 
 class ExtensionManager(commands.Cog):
