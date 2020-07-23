33259237f301b7ff2b50f82233d7476902715617
Dennis Pham <dennis@dennispham.me>
2020-07-23 08:18:05 +0000

    Create custom help command

--- snek/exts/core/__init__.py
+++ snek/exts/core/__init__.py
@@ -1,7 +1,9 @@
 from snek.bot import Snek
 from snek.exts.core.error_handler import ErrorHandler
+from snek.exts.core.help_command import Help
 
 
 def setup(bot: Snek) -> None:
     """Load the core cogs."""
     bot.add_cog(ErrorHandler(bot))
+    bot.add_cog(Help(bot))
