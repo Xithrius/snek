75720c821f712512ad4c141607525c4cc52d5168
Dennis Pham <dennis@dennispham.me>
2020-07-24 17:23:42 +0000

    Implement the Config cog

--- snek/exts/config.py
+++ snek/exts/config.py
@@ -1,10 +1,23 @@
 import logging
+import typing as t
 
-from discord.ext.commands import Cog, Context, group
+from discord.ext.commands import Cog, Context, group, RoleConverter
 
 from snek.bot import Snek
 
 log = logging.getLogger(__name__)
+
+CONFIG_KEYS = ('mod_role', 'admin_role')
+
+
+async def convert_config_value(ctx: Context, key: str, value: str) -> t.Any:
+    """Converts a config value according to its key."""
+    if key not in CONFIG_KEYS:
+        raise ValueError('❌ There is no such config key.')
+
+    if key in ('mod_role', 'admin_role'):
+        role_converter = RoleConverter()
+        return (await role_converter.convert(ctx, value)).id
 
 
 class Config(Cog):
@@ -13,7 +26,7 @@ class Config(Cog):
     def __init__(self, bot: Snek) -> None:
         self.bot = bot
 
-    @group(name='config', invoke_without_command=True)
+    @group(name='config', aliases=('conf',), invoke_without_command=True)
     async def config_group(self, ctx: Context) -> None:
         """Get, set, and reset the values in a guild's config."""
         await ctx.send_help(ctx.command)
@@ -21,14 +34,40 @@ class Config(Cog):
     @config_group.command(name='set', aliases=('s',))
     async def set_command(self, ctx: Context, key: str, value: str) -> None:
         """Set a value for `key` in for a guild's config."""
+        try:
+            value = await convert_config_value(ctx, key, value)
+        except ValueError as err:
+            await ctx.send(str(err))
+            return
+        else:
+            await self.bot.api_client.patch(
+                f'guild_configs/{ctx.guild.id}',
+                json={key: value}
+            )
+            await ctx.send(f'✅ Config key `{key}` successfully updated.')
 
     @config_group.command(name='get', aliases=('g',))
-    async def get_command(self, ctx: Context, key: str, value: str) -> None:
+    async def get_command(self, ctx: Context, key: str) -> None:
         """Get the value of `key` from a guild's config."""
+        if key not in CONFIG_KEYS:
+            await ctx.send('❌ There is no such config key.')
+            return
+
+        config = await self.bot.api_client.get(f'guild_configs/{ctx.guild.id}')
+        await ctx.send(f'The value of `{key}` is `{config[key]}`.')
 
     @config_group.command(name='reset', aliases=('r',))
     async def reset_command(self, ctx: Context, key: str) -> None:
         """Reset the value of `key` in a guild's config."""
+        if key not in CONFIG_KEYS:
+            await ctx.send('❌ There is no such config key.')
+            return
+
+        await self.bot.api_client.patch(
+            f'guild_configs/{ctx.guild.id}',
+            json={key: None}
+        )
+        await ctx.send(f'✅ Config key `{key}` was sucessfully reset.')
 
 
 def setup(bot: Snek) -> None:
