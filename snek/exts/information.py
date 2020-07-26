47b58b711c5b94ee518b93f2dcd031ce88e1064e
Dennis Pham <dennis@dennispham.me>
2020-07-26 04:11:00 +0000

    Information: Remove "saved" word

--- snek/exts/information.py
+++ snek/exts/information.py
@@ -211,7 +211,7 @@ class Information(Cog):
             user = await self.bot.api_client.get(f'users/{user}')
             created = dateutil.parser.isoparse(user['created_at']).replace(tzinfo=None)
 
-            username = f'{user["name"]} (saved)'
+            username = user['name']
 
             created_delta = humanize.precisedelta(created, minimum_unit='days', format=r'%0.0f', )
             created_at = datetime.strftime(created, r'%B %m, %Y')
