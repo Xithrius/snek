858234a73e954ac942124b5eab0432b1200fc5fc
Dennis Pham <dennis@dennispham.me>
2020-07-29 00:24:30 +0000

    Scheduler: Fix Task type hints

--- snek/utils/scheduler.py
+++ snek/utils/scheduler.py
@@ -25,13 +25,13 @@ class Scheduler:
         """Checks whether or not `task_id` is currently scheduled."""
         return task_id in self.tasks
 
-    def __getitem__(self, task_id: t.Hashable) -> t.Coroutine:
+    def __getitem__(self, task_id: t.Hashable) -> asyncio.Task:
         """Returns the coroutine mapped to `task_id`; Raises a `KeyError` if not found."""
         return self.tasks[task_id]
 
-    def __setitem__(self, task_id: t.Hashable, coroutine: t.Coroutine) -> None:
+    def __setitem__(self, task_id: t.Hashable, task: asyncio.Task) -> None:
         """Sets the coroutine to the `task_id`."""
-        self.tasks[task_id] = coroutine
+        self.tasks[task_id] = task
 
     def get(self, task_id: t.Hashable) -> t.Coroutine:
         """Returns the coroutine mapped to `task_id`; Returns `None` if not found."""
